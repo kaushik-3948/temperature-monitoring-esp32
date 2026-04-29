30adffa5b88e642eb8944d7577e5a936ca6373c3
kaushik-3948 <kaushi3948@gmail.com>
2026-04-29 13:18:54 +0530

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,18 @@
+#include "DHT.h"
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float temp = dht.readTemperature();
+  Serial.print("Temperature: ");
+  Serial.println(temp);
+  delay(2000);
+}
